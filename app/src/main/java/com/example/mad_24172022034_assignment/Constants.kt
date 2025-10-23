670d6fd8a4b46f0f4823374204de90592f9b9432
prit <132353280+Pritpatel11@users.noreply.github.com>
2025-10-23 09:04:03 +0530

    Update API key in Constants.kt

--- app/src/main/java/com/example/mad_24172022034_assignment/Constants.kt
+++ app/src/main/java/com/example/mad_24172022034_assignment/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.mad_24172022034_assignment
 
 object Constants {
-    const val apikey =  "AIzaSyBAH0z4ZpPbBq2YNZ4BbMatmfTnlU0-MxE"
+    const val apikey =  "Your api key...."
 }
